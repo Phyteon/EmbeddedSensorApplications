3f0b848f663e580e9c000a9ac777bc9c7ddf4ecb
Filip Pawelec <pawelecfilip6@gmail.com>
2022-07-03 23:47:25 +0200

    Draft of code layout.

---
+++ code/TMD/lib/comm_abstraction_layer/comm_abstraction_layer.hpp
@@ -0,0 +1,17 @@
+
+#ifndef __COMM_ABSTRACTION_LAYER__
+#define __COMM_ABSTRACTION_LAYER__
+
+extern "C" {
+    #include "esp_wifi.h"
+}
+
+namespace comm_ab_layer {
+    namespace wifi {
+        class WIFI {
+            
+        };
+    }
+}
+
+#endif /* __COMM_ABSTRACTION_LAYER__ */
